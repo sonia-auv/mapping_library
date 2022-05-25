1cebc6448e734b58135ddd107958947294c66277
Alexandre Desgagné <alexandre1998@live.ca>
2022-05-25 17:46:27 -0400

    Able to create planes from point cloud

--- Mapper.m
+++ Mapper.m
@@ -12,8 +12,8 @@ classdef Mapper < handle
         %% Mapper Constructor
         function this = Mapper()           
             % this.map3D = occupancyMap3D(10);
-            this.bigCloud = pointCloud(zeros([1, 3]));
-            this.player = pcplayer([0 30],[-30 30],[0 5]);
+            this.bigCloud = pointCloud(zeros([1, 3]), 'Intensity', 0);
+            this.player = pcplayer([-20 20],[-20 20],[0 5], 'VerticalAxisDir','Down');
         end
 
         %% Sonar laser scan callback.
@@ -32,21 +32,35 @@ classdef Mapper < handle
 
                quat(1) = poseSub.LatestMessage.Orientation.W;
                % Quaternion conjugate.
-               quat(2) = -poseSub.LatestMessage.Orientation.X;
-               quat(3) = -poseSub.LatestMessage.Orientation.Y;
-               quat(4) = -poseSub.LatestMessage.Orientation.Z;
+               quat(2) = poseSub.LatestMessage.Orientation.X;
+               quat(3) = poseSub.LatestMessage.Orientation.Y;
+               quat(4) = poseSub.LatestMessage.Orientation.Z;
             end
-            cartesians = rosReadXYZ(sonarMsg);
-            intensities = rosReadField(sonarMsg, 'intensity');
+            xyzi(:, 1:3) = rosReadXYZ(sonarMsg);
+            
+            % Temporary swap.
+            v = xyzi(:, 1);
+            xyzi(:, 1) = xyzi(:, 2);
+            xyzi(:, 2) = v;
+            
+%             ptCloud = pointCloud(cartesians);
+%             view(this.player, ptCloud);
 
-            xyzPoints = zeros([size(cartesians, 1), 3]);
-            for i=1:size(cartesians, 1)
-                point = sonar2NED(pos.', quat, [0.358, 0, -0.118].', [cartesians(i,1), cartesians(i,2), 0]).'; 
+            xyzi(:, 4) = rosReadField(sonarMsg, 'intensity');
+
+            rowsToDelete = any(xyzi(:,4) < 0.07 & norm(xyzi(:,1:3)) > 5.0, 2);
+            xyzi(rowsToDelete, :) = [];
+
+            xyzPoints = zeros([size(xyzi, 1), 3]);
+            for i=1:size(xyzi, 1)
+                point = sonar2NED(pos.', quat, [0.358, 0, -0.118].', [xyzi(i,1), xyzi(i,2), 0]).'; 
                 xyzPoints(i, :) = point(1:3);
             end
-            ptCloud = pointCloud(xyzPoints, 'Intensity', intensities);   
-            this.bigCloud = pccat([this.bigCloud, ptCloud]);
+            ptCloud = pointCloud(xyzPoints, 'Intensity', xyzi(:, 4));   
+            % this.bigCloud = pccat([this.bigCloud, ptCloud]);
+            this.bigCloud = pcmerge(this.bigCloud, ptCloud, 0.01);
             view(this.player, this.bigCloud);
+            
             % this.bigCloud = pcmerge(this.bigCloud, ptCloud, 1);
             % insertPointCloud(this.map3D, [0,0,0,1,0,0,0], ptCloud, double(sonarMsg.RangeMax));
         end
--- proc_mapping.m
+++ proc_mapping.m
@@ -1,8 +1,9 @@
 function proc_mapping
     % Global variables
-    global newSonarMsg newSonarPtMsg;
+    global newSonarMsg newSonarPtMsg bundleStarted;
     newSonarMsg = false;
     newSonarPtMsg = false;
+    bundleStarted = false;
     
     % Variables
     rosSpin = 10;
@@ -11,9 +12,9 @@ function proc_mapping
  
     % Subscribers
     poseSub = rossubscriber('/proc_nav/auv_pose', 'geometry_msgs/Pose', "DataFormat", "struct");
+    startStopSub = rossubscriber('/proc_mapping/start_stop', 'std_msgs/Bool', @startStopCallback, "DataFormat", "struct");
     sonarSub = rossubscriber('/provider_sonar/point_cloud2', 'sensor_msgs/PointCloud2', @sonarCallback, "DataFormat", "struct");
     
-    % Objects
     mapper = Mapper();
     
     fprintf('INFO : proc mapping : Node is started \n');
@@ -22,11 +23,21 @@ function proc_mapping
     
     while ~killNode
         % Add the laser scan to the point cloud when received.
-        if newSonarMsg
+        if newSonarMsg && bundleStarted
             mapper.add2PtCloud(sonarSub.LatestMessage, poseSub);
             newSonarMsg = false;
         end
         waitfor(r);
+    end
+end
+
+function startStopCallback(src, msg)
+    global bundleStarted;
+    bundleStarted = msg.Data;
+    if bundleStarted
+        fprintf('INFO : proc mapping : Bundle record started \n');
+    else
+        fprintf('INFO : proc mapping : Bundle record stopped \n');
     end
 end
 
